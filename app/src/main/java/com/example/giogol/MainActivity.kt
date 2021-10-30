ea5ea9de8e5a75bb9935f313e31f52434b3556b4
Giovanny1033 <gzamora18@uan.edu.co>
2021-10-29 22:11:49 -0500

    Firebase Inicio sesión

--- app/src/main/java/com/example/giogol/MainActivity.kt
+++ app/src/main/java/com/example/giogol/MainActivity.kt
@@ -5,16 +5,30 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.giogol.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.io.PrintStream
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
       /*  readNames()
         writeNames()*/
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
 
         val nextButton = findViewById<Button>(R.id.nextButton)
         nextButton.setOnClickListener {goToSecondActivity()
